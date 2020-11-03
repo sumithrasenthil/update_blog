d1fce847f729e9ed8772051f59ec9c287dd0f9e6
sumisenthil <sumithrasenthil1410@gmail.com>
2020-11-03 11:52:37 +0530

    removed the password field

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :user, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" , :edit => "/edit"}
+  devise_for :user, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" , :edit => "/edit"} 
+
 
   root to: "articles#index"
   resources :articles do
